da5e1abba26a4a7556707870f4b43fb11cacc02b
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-14 20:29:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Return the Remainder from Two Numbers
+
+// module operator %
+
+function remainder( num1, num2) {
+    return num1 % num2;
+}
+console.log(remainder(1, 3));
+console.log(remainder(3, 4));
+console.log(remainder(-9, 45));
+console.log(remainder(5, 5));
